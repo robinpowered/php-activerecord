823729da19357b65933d06324aa4561d9d2a1946
Trevor Suarez <rican7@gmail.com>
2016-06-06 17:58:13 -0400

    Implementing the `__clone()` method with the proper model dereferencing logic

--- lib/DateTime.php
+++ lib/DateTime.php
@@ -120,6 +120,20 @@ class DateTime extends \DateTime
 		return $this->format();
 	}
 
+	/**
+	 * Handle PHP object `clone`.
+	 *
+	 * This makes sure that the object doesn't still flag an attached model as
+	 * dirty after cloning the DateTime object and making modifications to it.
+	 *
+	 * @return void
+	 */
+	public function __clone()
+	{
+		$this->model = null;
+		$this->attribute_name = null;
+	}
+
 	private function flag_dirty()
 	{
 		if ($this->model) {
