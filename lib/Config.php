081a8eaeaa03d83b4e2c816a480503fecec57535
Trevor Suarez <rican7@gmail.com>
2017-08-11 11:28:04 -0600

    Now using strong typing for the logger config

--- lib/Config.php
+++ lib/Config.php
@@ -228,17 +228,12 @@ class Config extends Singleton
 	/**
 	 * Sets the logger object for future SQL logging
 	 *
-	 * @param object $logger
+	 * @param Logger $logger
 	 * @return void
 	 * @throws ConfigException if Logger objecct does not implement public log()
 	 */
-	public function set_logger($logger)
+	public function set_logger(Logger $logger)
 	{
-		$klass = Reflections::instance()->add($logger)->get($logger);
-
-		if (!$klass->getMethod('log') || !$klass->getMethod('log')->isPublic())
-			throw new ConfigException("Logger object must implement a public log method");
-
 		$this->logger = $logger;
 	}
 
@@ -255,7 +250,7 @@ class Config extends Singleton
 	/**
 	 * Returns the logger
 	 *
-	 * @return object
+	 * @return Logger
 	 */
 	public function get_logger()
 	{
