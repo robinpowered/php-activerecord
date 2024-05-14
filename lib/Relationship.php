434a4f730fa8a2f77e51797003f5ddc6c4650139
Jacob Struzik <jake@robinpowered.com>
2024-05-14 09:29:24 -0700

    Switch to is array check

--- lib/Relationship.php
+++ lib/Relationship.php
@@ -302,7 +302,7 @@ abstract class AbstractRelationship implements InterfaceRelationship
 
 	protected function create_conditions_from_keys(Model $model, $condition_keys=[], $value_keys=array())
 	{
-		$condition_string = implode('_and_', is_string($condition_keys) ? array($condition_keys) : $condition_keys);
+		$condition_string = implode('_and_', is_array($condition_keys) ? $condition_keys : [$condition_keys]);
 		$condition_values = array_values($model->get_values_for($value_keys));
 
 		// return null if all the foreign key values are null so that we don't try to do a query like "id is null"
